49f1c6a5ba95fff04725b928101770910178c9bb
Ronit1605 <ronit.tayade@embarkingonvoyage.com>
2026-07-10 10:29:36 +0530

    improved ingestion for table data

--- app/ingest_table.py
+++ app/ingest_table.py
@@ -12,13 +12,18 @@ OLLAMA_URL = os.getenv("OLLAMA_URL", "http://localhost:11434")
 COLLECTION_NAME = "local_rag"
 
 client=chromadb.HttpClient(host=CHROMA_HOST,port=CHROMA_PORT)
-embeddings = OllamaEmbeddings(model="nomic-embed-text")
+# try:                                            #to clear the existing collection before ingesting new data
+#     client.delete_collection("local_rag")
+#     print("Cleared existing collection.")
+# except Exception:
+#     print("Collection did not exist yet. Creating fresh.")
+embeddings = OllamaEmbeddings(base_url=OLLAMA_URL, model="nomic-embed-text", embed_instructions="search_document: ")
 
 def ingest_table():
     target_path = os.path.join("..", "data")
-    pdf_path = os.path.join(target_path,"LOINC_1.6_Top2000CommonLabResultsUS.pdf")
+    pdf_path = os.path.join(target_path,"PDF_FILE.pdf")
     docs = []
-    loinc_pattern = re.compile(r"^(\d{4,5}-\d)\s+(.*)")
+    # loinc_pattern = re.compile(r"^(\d{4,5}-\d)\s+(.*)")
     with pdfplumber.open(pdf_path) as pdf:
         for page_num, page in enumerate(pdf.pages[2:], start=3):
             tables = page.extract_tables()
@@ -28,16 +33,21 @@ def ingest_table():
                 for row in table[1:]:
                     if not row[1]:
                         continue
+                    
                     text = row[1].strip()
-                    match = loinc_pattern.match(text)
-                    loinc_code = None
-                    if match:
-                        loinc_code = match.group(1)
+                    if len(text)>300:
+                        continue
+                    if len(text)<300 and len(text)>60:
+                        text=text[:200]
+                    # match = loinc_pattern.match(text)
+                    # loinc_code = None
+                    # if match:
+                        # loinc_code = match.group(1)
                     docs.append(
                         Document(
                             page_content=text,
                             metadata={
-                                "loinc": loinc_code,
+                                # "loinc": loinc_code,
                                 "page": page_num,
                                 "rank": row[0]}))
         print("Docs created:", len(docs))
